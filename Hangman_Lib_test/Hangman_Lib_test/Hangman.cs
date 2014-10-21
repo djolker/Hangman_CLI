aa0f28e3fcc87e103ce02d9766270c48e102385e
djolker <dj.olker@gmail.com>
2014-10-21 13:45:56 -0500

    Adding project files

---
+++ Hangman_Lib_test/Hangman_Lib_test/Hangman.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hangman_Lib_test
+{
+    /// <summary>
+    /// Hangman Game Object
+    /// </summary>
+    class Hangman
+    {
+        #region variables
+        /// <summary>
+        /// Goal word
+        /// </summary>
+        private char[] word {get; set;}
+
+        /// <summary>
+        /// players progress towards the goal word
+        /// </summary>
+        public List<char> fillingWord = new List<char>();
+
+        /// <summary>
+        /// letters already guessed along the way
+        /// </summary>
+        public List<char> guessedLetters = new List<char>();
+
+        /// <summary>
+        /// int16 to represent number of strikes
+        /// </summary>
+        public Int16 strikes = new short();
+
+        /// <summary>
+        /// Dictionary pulled from dictionary.txt file
+        /// </summary>
+        private List<string> dictionary = new List<string>();
+
+        /// <summary>
+        /// Status of the game was lost or not. 
+        /// </summary>
+        private bool gameOver = false;
+
+        #endregion
+
+        /// <summary>
+        /// Start new game of Hangman
+        /// </summary>
+        /// <param name="difficulty"></param>
+        public Hangman(int difficulty)
+        {
+            gameOver = false;
+            loadDictionary();
+            initNewWord(difficulty);
+        }
+
+        public bool getGameOverStatus()
+        {
+            return gameOver;
+        }
+
+        /// <summary>
+        /// Takes in int representing difficulty and finds a word that suits that choice, then applies it to variable word
+        /// </summary>
+        /// <param name="difficulty"></param>
+        private void initNewWord(int difficulty)
+        {
+            Random rnd = new Random();
+
+            int min = new int();
+            int max = new int();
+
+            switch (difficulty)
+            {
+                case 1:
+                    min = 0;
+                    max = 4;
+                    break;
+                case 2:
+                    min = 5;
+                    max = 6;
+                    break;
+                case 3:
+                    min = 7;
+                    max = 8;
+                    break;
+                case 4:
+                    min = 9;
+                    max = 10000;
+                    break;
+            }
+
+            while (true)
+            {
+                int randomNum = rnd.Next(0, dictionary.Count);
+                if (min <= dictionary[randomNum].Length && dictionary[randomNum].Length <= max)
+                {
+                    word = dictionary[randomNum].ToCharArray();
+                    break;
+                }
+            }
+
+            for (int i = 0; i < word.Count(); i++)
+            {
+                fillingWord.Add('_');
+            }
+        }
+
+        /// <summary>
+        /// loads dictionary file into list
+        /// </summary>
+        /// <returns></returns>
+        private void loadDictionary()
+        {
+            dictionary = File.ReadAllLines("dictionary.txt").ToList();
+        }
+
+        /// <summary>
+        /// Letter is taken in, checked against the goal word, puts letters into fillingWords array
+        /// </summary>
+        /// <param name="letter"></param>
+        public void guessLetter(char letter)
+        {
+            if (!guessedLetters.Contains(letter))
+            {
+                if (word.Contains(letter))
+                {
+                    for (int i = 0; i < word.Count(); i++)
+                    {
+                        if (word[i] == letter)
+                        {
+                            fillingWord[i] = letter;
+                        }
+
+                        guessedLetters.Add(letter);
+                    }
+                }
+                else
+                {
+                    strikes++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Letter " + letter.ToString() + " has already been guessed");
+                Console.Read();
+            }
+        }
+
+        /// <summary>
+        /// starts new round with current progress
+        /// </summary>
+        public void newRound()
+        {
+            Console.Clear();
+            
+            if (checkForCompletion())
+            {
+                Console.WriteLine("Grats you win");
+                Console.ReadKey();
+            }
+            else if (checkForGameOver())
+            {
+                Console.WriteLine("Too many strikes, Game Over");
+
+                StringBuilder completeword = new StringBuilder();
+                foreach (char letter in word)
+                {
+                    completeword.Append(letter.ToString());  
+                }
+                
+                Console.WriteLine("The word was " + completeword.ToString());
+                Console.WriteLine("Press Enter to Continue...");
+                Console.Read();
+            }
+            else
+            {
+                showProgress();
+
+                Console.WriteLine("What is your next guess?");
+                guessLetter(Console.ReadKey().KeyChar);
+            }
+        }
+
+        /// <summary>
+        /// Checks for strikes based GameOver
+        /// </summary>
+        /// <returns></returns>
+        private bool checkForGameOver()
+        {
+            if (strikes >= 6)
+            {
+                gameOver = true;
+                
+            }
+            return gameOver;
+        }
+
+        /// <summary>
+        /// Checks for a GameWin
+        /// </summary>
+        /// <returns>GameWin Bool</returns>
+        public bool checkForCompletion()
+        {
+            bool gameWon = true;
+
+            for (int i = 0; i < word.Count(); i++)
+            {
+                if (!word[i].Equals(fillingWord[i]))
+                {
+                    gameWon = false;
+                    break;
+                }
+            }
+
+            return gameWon;
+        }
+
+        /// <summary>
+        /// Prints current game status
+        /// </summary>
+        private void showProgress()
+        {
+            string currentProgress = ""; //recreated each cycle to show the current players progress
+            foreach (char letter in fillingWord)
+            {
+                if (!(letter.ToString() == @"\0"))
+                {
+                    currentProgress += " " + letter.ToString();
+                }
+                else
+                {
+                    currentProgress += " _ ";
+                }
+            }
+            Console.WriteLine("Current Progress: " + currentProgress);
+            Console.WriteLine("Strikes: " + strikes + "/6");
+        }
+    }
+}
---
+++ Hangman_Lib_test/Hangman_Lib_test/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+//using System.Web;
+
+//using IDM.SkPublish.API;
+//using System.Web.Script.Serialization;
+
+namespace Hangman_Lib_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wins = new int();
+            int loss = new int();
+
+            while (true) //Game
+            {
+                Console.Clear();
+
+                Console.WriteLine("Wins: " + wins.ToString());
+                Console.WriteLine(@"Loss's: " + loss.ToString());
+                Console.WriteLine("");
+                Console.WriteLine("Choose a difficulty");
+                Console.WriteLine("1. Easy           1-4 Letters");
+                Console.WriteLine("2. Medium         5-6 Letters");
+                Console.WriteLine("3. Hard           7-8 Letters");
+                Console.WriteLine("4. Expert         9+  Letters");
+
+                int diffchoice = Convert.ToInt32((Console.ReadKey().KeyChar).ToString());
+
+                Hangman game = new Hangman(diffchoice);
+
+                while (true) //Round
+                {
+                    if (game.getGameOverStatus())
+                    {
+                        loss++;
+                        break;
+                    }
+                    if (game.checkForCompletion())
+                    {
+                        wins++;
+                        break;
+                    }
+
+                    game.newRound();
+                }
+
+                Console.Clear();
+                Console.WriteLine("Play again? Y/N");
+
+                char choice = Console.ReadKey().KeyChar;
+                if (choice == 'y')
+                {
+                    continue;
+                }
+                else if (choice == 'n')
+                {
+                    break;
+                }
+            }
+        }
+
+        #region Test Code
+        /// <summary>
+        /// Pulls dictionary of words from Cambridge API
+        /// </summary>
+        //static void getDictionary()
+        //{
+        //    string baseUrl = @"https://dictionary.cambridge.org/";
+        //    string accessKey = "AB0uPWh2AzwtaQptKSdRYAGOgoXXZsPHc8VzSo1tnCeKTcHXPSfqBgWN18T6vcoe";
+                
+        //    SkPublishAPI api = new SkPublishAPI(baseUrl + "/api/v1/", accessKey);
+        //    IDictionary<string, object> results = JsonToObject(api.GetNearbyEntries("american-english", "circa", 5));
+
+        //    int x = 0;
+        //}
+
+        ///// <summary>
+        ///// Written by Cambridge Software Developer
+        ///// </summary>
+        ///// <param name="json"></param>
+        ///// <returns></returns>
+        //private static IDictionary<string, object> JsonToObject(string json)
+        //{
+        //    JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+        //    IDictionary<string, object> obj = jsonSerializer.Deserialize<IDictionary<string, object>>(json);
+        //    return obj;
+        //}
+#endregion
+    }
+}
